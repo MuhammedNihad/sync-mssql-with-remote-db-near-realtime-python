09ee33860deeaeddd9600a01685b6b958dc5fa23
Muhammed Nihad <muhammednihad@pm.me>
2022-12-06 12:06:20 +0530

    feat: convert query results to list of dict to send through API

--- script.py
+++ script.py
@@ -21,10 +21,23 @@ else:
     last_executed_time = five_minutes_ago.isoformat(timespec='milliseconds')
 
     try:
-        # Query to fetch rows that are modified within 5 minutes ago
+        # Execute query to fetch rows that are modified within 5 minutes ago
         cursor = conn.cursor().execute(f"SELECT name, code, price, stock FROM {config('TABLE_NAME')} WHERE modified_at > '{last_executed_time}'")
     except Exception as error:
         print(f"Failed to execute query. \n{error}")
     else:
-        for data in cursor:
-            print(data)
+        results = []
+
+        """
+        Converting query result as list of key value pairs which is the desired format for sending through API.
+        Otherwise, looping through data in cursor will be in tuple without column name. eg: ('milk','M23',19.90,10)
+        """
+        # Fetch all columns in the query result
+        columns = [column[0] for column in cursor.description]
+        # Fetch all rows in the query result
+        for row in cursor.fetchall():
+            # Append all columns and rows as dict into results list.
+            # eg: [{'name':'milk','code':'M23','price':19.90,'stock':10}]
+            results.append(dict(zip(columns, row)))
+
+        print(results)
