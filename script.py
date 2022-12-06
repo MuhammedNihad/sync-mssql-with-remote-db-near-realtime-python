33d20450a6b4c27126b85c7613e9d07f7093cd4c
Muhammed Nihad <muhammednihad@pm.me>
2022-12-06 12:01:13 +0530

    refactor: handle exception of query execution

--- script.py
+++ script.py
@@ -20,6 +20,11 @@ else:
     # Strip last 3 string by converting to isoformat for matching time string with db time string
     last_executed_time = five_minutes_ago.isoformat(timespec='milliseconds')
 
-    cursor = conn.cursor().execute(f"SELECT name, code, price, stock FROM {config('TABLE_NAME')} WHERE modified_at > '{last_executed_time}'")
-    for data in cursor:
-        print(data)
+    try:
+        # Query to fetch rows that are modified within 5 minutes ago
+        cursor = conn.cursor().execute(f"SELECT name, code, price, stock FROM {config('TABLE_NAME')} WHERE modified_at > '{last_executed_time}'")
+    except Exception as error:
+        print(f"Failed to execute query. \n{error}")
+    else:
+        for data in cursor:
+            print(data)
