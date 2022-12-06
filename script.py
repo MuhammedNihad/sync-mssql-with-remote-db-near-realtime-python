f5289fd5c488a83e491ad73bf291135941899a2c
Muhammed Nihad <muhammednihad@pm.me>
2022-12-06 11:53:35 +0530

    feat: fetch rows that are modified in last five minutes

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from decouple import config
 import pyodbc
 
@@ -14,6 +15,11 @@ try:
 except Exception as error:
     print(f"Connection failed. \n{error}")
 else:
-    cursor = conn.cursor().execute(f"SELECT * FROM {config('TABLE_NAME')}")
+    # Assign 5 minutes ago explicitly as last executed time
+    five_minutes_ago = datetime.now() - timedelta(minutes=5)
+    # Strip last 3 string by converting to isoformat for matching time string with db time string
+    last_executed_time = five_minutes_ago.isoformat(timespec='milliseconds')
+
+    cursor = conn.cursor().execute(f"SELECT name, code, price, stock FROM {config('TABLE_NAME')} WHERE modified_at > '{last_executed_time}'")
     for data in cursor:
         print(data)
